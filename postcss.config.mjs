376e05b4c3bdf425d0b34707f8d3679e24888d81
Christopher Costa <contato.christopher@gmail.com>
2025-06-05 00:25:28 -0300

    chore: add webpack

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,5 @@
 const config = {
-  plugins: ["@tailwindcss/postcss", "autoprefixer", "tailwindcss", "postcss-import"],
+  plugins: ["@tailwindcss/postcss", "autoprefixer"],
 };
 
 export default config;
