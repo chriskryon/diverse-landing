bc2d9556f21731af4f75439d6631ddb70f464ccd
Christopher Costa <contato.christopher@gmail.com>
2025-06-10 14:46:42 -0300

    style: refactor tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,12 @@ import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
 
 const config: Config = {
-	darkMode: "class",
+	darkMode: false,
 	content: [	
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/**/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/*/*/*.{js,ts,jsx,tsx,mdx}",
-		"./src/*/*/*/*.{js,ts,jsx,tsx,mdx}",
-		"./src/*/*/*/*/*.{js,ts,jsx,tsx,mdx}",
+		 "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"*.{js,ts,jsx,tsx,mdx}",
 		{ raw: "bg-gradient-to-(r|l|t|b)" },
 		{ raw: "from-(yellow|pink)" },
